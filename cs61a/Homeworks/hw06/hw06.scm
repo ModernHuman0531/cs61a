d55140e42ee8a5465d9c41d0b77712f6b77c498e
ModernHuman0531 <dunkun130897441@gmail.com>
2024-11-01 21:52:51 +0800

    finish hw06

--- cs61a/Homeworks/hw06/hw06.scm
+++ cs61a/Homeworks/hw06/hw06.scm
@@ -3,15 +3,22 @@
 
 (define (cadr s)
   'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
   'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 
 (define (sign num)
   'YOUR-CODE-HERE
+  (cond
+    ((< num 0) -1)
+    ((= num 0) 0)
+    (else 1)
+  )
 )
 
 
@@ -19,5 +26,13 @@
 
 (define (pow x y)
   'YOUR-CODE-HERE
+  (if (= y 0)
+    1
+    (if (odd? y)
+      (* x (square (pow x (quotient (- y 1) 2))))
+      (* 1 (square (pow x (quotient y 2))))
+    )
+  
+  )
 )
 
