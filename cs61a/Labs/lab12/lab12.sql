007fbb6aeb617dc8ad9e257a3acda10dd1468a12
ModernHuman0531 <dunkun130897441@gmail.com>
2024-11-11 20:20:23 +0800

    finish lab12

--- cs61a/Labs/lab12/lab12.sql
+++ cs61a/Labs/lab12/lab12.sql
@@ -2,20 +2,26 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet from from students
+    where color = 'blue' and pet = 'dog';
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song from students
+    where color = 'blue' and 'dog';
 
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest from students
+    where smallest > 2 and order by smallest linit = 20;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.color, a.music, a.pet, b.color
+    from students as a, students as b
+      where a.music = b.music and a.pet = b,pet and a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven from students as a, numbers as b 
+    where a.time = b.time and a.number = 7 and b.'7' = 'True'
 
